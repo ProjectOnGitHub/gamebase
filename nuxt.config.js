e0a187519c2157bb9c73b76e6b826361f70670df
Aleksandr <soft@vhlv.ru>
2023-02-27 15:48:55 +0300

    initital commit

---
+++ nuxt.config.js
@@ -0,0 +1,74 @@
+export default {
+  /*
+   ** Nuxt rendering mode
+   ** See https://nuxtjs.org/api/configuration-mode
+   */
+  mode: 'universal',
+  /*
+   ** Nuxt target
+   ** See https://nuxtjs.org/api/configuration-target
+   */
+  target: 'server',
+  /*
+   ** Headers of the page
+   ** See https://nuxtjs.org/api/configuration-head
+   */
+  head: {
+    title: 'Наш проект для НКО',
+    meta: [
+      {charset: 'utf-8'},
+      {name: 'viewport', content: 'width=device-width, initial-scale=1'},
+      {
+        hid: 'description',
+        name: 'description',
+        content: process.env.npm_package_description || '',
+      },
+    ],
+    link: [{rel: 'icon', type: 'image/x-icon', href: '/favicon.ico'}],
+    link: [
+      {
+        rel: 'stylesheet',
+        href: 'https://fonts.googleapis.com/css2?family=Roboto:wght@100;400;700&display=swap',
+      },
+    ],
+  },
+  /*
+   ** Global CSS
+   */
+  css: ['@/assets/normalize.css', '@/assets/scss/_variables.scss', '@/assets/scss/_mixins.scss'],
+  /*
+   ** Plugins to load before mounting the App
+   ** https://nuxtjs.org/guide/plugins
+   */
+  plugins: [],
+  /*
+   ** Auto import components
+   ** See https://nuxtjs.org/api/configuration-components
+   */
+  components: true,
+  /*
+   ** Nuxt.js dev-modules
+   */
+  buildModules: [],
+  /*
+   ** Nuxt.js modules
+   */
+  modules: ['@nuxtjs/style-resources'],
+  /*
+   ** Build configuration
+   ** See https://nuxtjs.org/api/configuration-build/
+   */
+
+  styleResources: {
+    scss: ['./assets/scss/*.scss'],
+  },
+  build: {
+    postcss: {
+      preset: {
+        autoprefixer: {
+          grid: true,
+        },
+      },
+    },
+  },
+};
---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <h1 class="title">Тест</h1>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+.title {
+  font-size: 25px;
+  @include flexible(100%);
+}
+</style>
