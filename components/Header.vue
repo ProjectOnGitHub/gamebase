ba53d0680899708aa88bea829d24c3a0a8d5627d
Aleksandr <soft@vhlv.ru>
2023-02-28 13:52:45 +0300

    style: add style for Header

--- components/Header.vue
+++ components/Header.vue
@@ -24,4 +24,21 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.header {
+  @include gridable(100%);
+  justify-items: center;
+  box-sizing: border-box;
+  &__container {
+    @include gridable(100%);
+    grid-template-columns: repeat(2, min-content);
+    justify-content: space-between;
+    align-items: center;
+    box-sizing: border-box;
+    max-width: $max-width;
+    padding: 10px 20px;
+    gap: 20px 0;
+    height: $header-height;
+  }
+}
+</style>
