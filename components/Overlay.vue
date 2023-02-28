aea4e21b4398da79b40ca48e1965b9733dbbf0d8
Aleksandr <soft@vhlv.ru>
2023-02-28 13:50:48 +0300

    style: add style for Overlay

--- components/Overlay.vue
+++ components/Overlay.vue
@@ -6,4 +6,28 @@
 export default {};
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.overlay {
+  display: none;
+  @media screen and (max-width: $tablet-small) {
+    position: fixed;
+    top: 0;
+    right: -150%;
+    z-index: 15;
+    display: block;
+    width: 100%;
+    height: 100vh;
+    background: $color-bg;
+    visibility: hidden;
+    opacity: 0;
+    transition: 0.5s;
+    &_active {
+      left: 0;
+      visibility: visible;
+      cursor: pointer;
+      opacity: 0.6;
+      transition: 0.5s;
+    }
+  }
+}
+</style>
