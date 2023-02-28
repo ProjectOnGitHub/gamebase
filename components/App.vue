c9275c89e6f227994dca5fb99f4284cbc1ff6765
Aleksandr <soft@vhlv.ru>
2023-02-28 13:43:23 +0300

    feat: create App

---
+++ components/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="app">
+    <slot />
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+.app {
+  @include gridable(100%);
+  font-family: 'Inter', Arial, sans-serif;
+  grid-template-columns: 1fr;
+  justify-items: center;
+  align-content: space-between;
+  box-sizing: border-box;
+  height: 100%;
+  background-color: $color-bg;
+  font-weight: 400;
+  line-height: 1.21;
+  font-style: normal;
+  color: $color-text-main;
+}
+</style>
