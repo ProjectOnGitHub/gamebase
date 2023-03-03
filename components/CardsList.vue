3d5e38830886193697b735d15489e64378a3c55a
Aleksandr <soft@vhlv.ru>
2023-03-03 21:35:18 +0300

    feat: create CardsList

---
+++ components/CardsList.vue
@@ -0,0 +1,25 @@
+<template>
+  <ul class="cards">
+    <Card />
+    <Card />
+    <Card />
+  </ul>
+</template>
+
+<script>
+import Card from './Card.vue';
+export default {
+  components: {
+    Card,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.cards {
+  @include gridable(100%);
+  @include unmarkedList;
+  grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
+  gap: 30px;
+}
+</style>
