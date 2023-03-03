a3ace0b1dd609b4131245ea217a870cf4583f33a
Aleksandr <soft@vhlv.ru>
2023-03-03 21:35:50 +0300

    feat: add components

--- components/Search.vue
+++ components/Search.vue
@@ -1,14 +1,18 @@
 <template>
   <section class="search">
     <SearchForm />
+    <h2 class="search__title">Результаты поиска</h2>
+    <CardsList />
   </section>
 </template>
 
 <script>
 import SearchForm from './SearchForm.vue';
+import CardsList from './CardsList.vue';
 export default {
   components: {
     SearchForm,
+    CardsList,
   },
 };
 </script>
@@ -16,9 +20,16 @@ export default {
 <style lang="scss" scoped>
 .search {
   @include gridable(100%);
+  gap: 20px;
   box-sizing: border-box;
   max-width: $max-width;
-  padding: 10px 20px;
+  padding: 35px 20px;
   justify-items: center;
+  &__title {
+    margin: 0;
+    font-weight: 500;
+    font-size: 28px;
+    padding-top: 15px;
+  }
 }
 </style>
