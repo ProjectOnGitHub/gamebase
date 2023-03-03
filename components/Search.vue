05cf71b71ae3dc175b45598cdc1d4a0ad3151649
Aleksandr <soft@vhlv.ru>
2023-03-03 17:07:08 +0300

    feat: create Search

---
+++ components/Search.vue
@@ -0,0 +1,23 @@
+<template>
+  <section class="search">
+    <SearchForm />
+  </section>
+</template>
+
+<script>
+import SearchForm from './SearchForm.vue';
+export default {
+  components: {
+    SearchForm,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.search {
+  @include gridable(100%);
+  box-sizing: border-box;
+  max-width: $max-width;
+  padding: 10px 20px;
+}
+</style>
