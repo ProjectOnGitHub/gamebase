e92de9d73929daa73394e5c8fef576bed6dc0b98
Aleksandr <soft@vhlv.ru>
2023-03-06 15:51:15 +0300

    style: add media queries for cards

--- components/CardsList.vue
+++ components/CardsList.vue
@@ -12,5 +12,14 @@
   @include unmarkedList;
   grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
   gap: 30px;
+  @media screen and (max-width: $desktop) {
+    grid-template-columns: repeat(3, minmax(200px, 1fr));
+  }
+  @media screen and (max-width: $tablet) {
+    grid-template-columns: repeat(2, minmax(200px, 1fr));
+  }
+  @media screen and (max-width: $smartphone) {
+    grid-template-columns: minmax(200px, 1fr);
+  }
 }
 </style>
