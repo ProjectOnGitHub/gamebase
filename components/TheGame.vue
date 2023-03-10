8df8d239e061de59ff605e00bd5cc59c0b804b6d
Aleksandr <soft@vhlv.ru>
2023-03-10 14:20:20 +0300

    style: add TheGame style

--- components/TheGame.vue
+++ components/TheGame.vue
@@ -1,28 +1,69 @@
 <template>
   <article class="game__article">
-    <h1 class="section__title game__title">
-      {{ $store.state.gameById.title }}
+    <h1 class="game__title">
+      {{ game.title }}
     </h1>
-    <p class="game__subtitle">
-      {{ $store.state.gameById.description }}
+    <p class="game__text game__text_small">
+      {{ game.short_description }}
     </p>
     <img
-      :src="$store.state.gameById.thumbnail"
-      :alt="$store.state.gameById.title"
-      class="game__image"
+      :src="game.thumbnail"
+      :alt="game.title"
+      class="game__thumbnail"
     />
-    <ul class="game__info">
-      <li class="game__info-item">Genre: {{ $store.state.gameById.genre }}</li>
-      <li class="game__info-item">Developer: {{ $store.state.gameById.developer }}</li>
-      <li class="game__info-item">Publiser: {{ $store.state.gameById.publisher }}</li>
-      <li class="game__info-item">Release: {{ $store.state.gameById.release_date }}</li>
-      <li class="game__info-item">Platform: {{ $store.state.gameById.platform }}</li>
+    <ul class="game__info game__info_additional">
+      <li class="game__info-item">
+        <span class="game__info-name">Genre:</span> {{ game.genre }}
+      </li>
+      <li class="game__info-item">
+        <span class="game__info-name">Developer:</span> {{ game.developer }}
+      </li>
+      <li class="game__info-item">
+        <span class="game__info-name">Publiser:</span> {{ game.publisher }}
+      </li>
+      <li class="game__info-item">
+        <span class="game__info-name">Release:</span> {{ game.release_date }}
+      </li>
+      <li class="game__info-item">
+        <span class="game__info-name">Platform:</span> {{ game.platform }}
+      </li>
+    </ul>
+    <ul class="game__info game__info_requirements">
+      <li
+        v-for="(value, name) in game.minimum_system_requirements"
+        :key="name"
+        class="game__info-item"
+      >
+        <span class="game__info-name">{{ name }}:</span>
+        {{ value }}
+      </li>
+    </ul>
+    <p class="game__text game__text_full">
+      {{ game.description }}
+    </p>
+    <ul class="game__screenshots">
+      <li
+        v-for="(screen, idx) in game.screenshots"
+        :key="idx"
+        class="game__screenshots-item"
+      >
+        <img
+          :src="screen.image"
+          :alt="`${game.title} screenshot id ${screen.id}`"
+          class="game__screenshot"
+        />
+      </li>
     </ul>
   </article>
 </template>
 
 <script>
 export default {
+  computed: {
+    game() {
+      return this.$store.getters.getCurrentGame;
+    }
+  },
   mounted() {
     const { id } = this.$route.params;
     this.$store.dispatch('getGameById', id);
@@ -30,4 +71,87 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.game {
+  &__article {
+    @include gridable(100%);
+    grid-template-columns: min-content 1fr;
+    gap: 15px 30px;
+    font-size: 16px;
+    grid-template-areas:
+      'thumbnail title'
+      'thumbnail text-small'
+      'thumbnail additional'
+      'requirements text-full'
+      'screens screens';
+    @media screen and (max-width: $tablet) {
+      grid-template-areas:
+        'title'
+        'text-small'
+        'thumbnail'
+        'additional'
+        'requirements'
+        'text-full'
+        'screens';
+      grid-template-columns: 1fr;
+    }
+  }
+  &__thumbnail {
+    grid-area: thumbnail;
+    @media screen and (max-width: $smartphone) {
+      width: 100%;
+      height: auto;
+    }
+  }
+  &__title {
+    grid-area: title;
+    font-size: 28px;
+    margin: 0;
+    font-weight: 500;
+  }
+  &__text {
+    margin: 0;
+    &_small {
+      grid-area: text-small;
+      font-size: 12px;
+    }
+    &_full {
+      grid-area: text-full;
+    }
+  }
+  &__info {
+    @include unmarkedList;
+    @include gridable(100%);
+    align-content: start;
+    gap: 5px;
+    &_additional {
+      grid-area: additional;
+    }
+    &_requirements {
+      grid-area: requirements;
+    }
+
+    &-item {
+      @include gridable(100%);
+      grid-template-columns: 120px 1fr;
+      gap: 15px;
+    }
+    &-name {
+      color: $color-link-main;
+      text-transform: uppercase;
+    }
+  }
+  &__screenshots {
+    @include unmarkedList;
+    @include gridable(100%);
+    grid-template-columns: repeat(auto-fill, minmax(250px, 1fr));
+    gap: 15px;
+    grid-area: screens;
+  }
+  &__screenshot {
+    width: 100%;
+    height: auto;
+    object-fit: contain;
+  }
+}
+</style>
