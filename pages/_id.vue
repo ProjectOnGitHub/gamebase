76ae27008edb26bd9bff7ef53bf043479346bc5a
Aleksandr <soft@vhlv.ru>
2023-04-10 12:47:18 +0300

    feat: add title for dynamic pages

--- pages/_id.vue
+++ pages/_id.vue
@@ -8,11 +8,35 @@
 </template>
 
 <script>
+import BaseSection from '../components/BaseSection.vue';
+import TheGame from '../components/TheGame.vue';
+import TheLoader from '../components/TheLoader.vue';
+
 export default {
+  components: { BaseSection, TheLoader, TheGame },
   data() {
     return {
       isShowLoader: false
     };
+  },
+  head() {
+    return {
+      title: this.game.title,
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        {
+          hid: 'description',
+          name: 'description',
+          content: this.game.short_description
+        }
+      ]
+    };
+  },
+  computed: {
+    game() {
+      return this.$store.state.gameById;
+    }
   },
   mounted() {
     this.toggleLoader();
